50d3d1c7f00d65668b64ab16e6af7cd72afe4a38
ImCocos <opustoshitel228@gmail.com>
2023-08-17 14:11:05 +0300

    returned

--- main.py
+++ main.py
@@ -58,28 +58,6 @@ class Server:
         bot = bot
         dp = dispatcher
 
-        @dp.message_handler(commands=['weather'])
-        async def weather_test(message: types.Message):
-            weather_data = requests.get(api_weather_url).content
-            weather_data = json.loads(weather_data)
-
-            temp = round(float(weather_data['main']['temp']) - 273, 2)
-            pressure = weather_data['main']['pressure']
-            humidity = weather_data['main']['humidity']
-            max_temp = round(float(weather_data['main']['temp_max']) - 273, 2)
-            min_temp = round(float(weather_data['main']['temp_min']) - 273, 2)
-            wind_speed = round(float(weather_data['wind']['speed']), 2)
-
-            weather_text = f'Погода на сегодня:\n'
-            weather_text += f'  Температура: {temp}°С\n'
-            weather_text += f'  Температура(макс): {max_temp}°С\n'
-            weather_text += f'  Температура(мин): {min_temp}°С\n'
-            weather_text += f'  Давление: {pressure}мм рт.ст.\n'
-            weather_text += f'  Влажность: {humidity}\n'
-            weather_text += f'  Скорость ветра: {wind_speed}м/с\n'
-
-            await message.reply(text=weather_text)
-
         @dp.message_handler(lambda message: 1 == 2)
         async def start(message: types.Message):
             while True:
@@ -138,7 +116,7 @@ class Server:
                     await bot.send_message(chat_id=-1001869856367, text=message_text, parse_mode='HTML')
                     self.flag.clear()
         
-        executor.start_polling(dispatcher=dp, skip_updates=True,)
+        executor.start_polling(dispatcher=dp, skip_updates=True, on_startup=start)
     
     def run(self):
         t1 = Thread(target=self.site)
