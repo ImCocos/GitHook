4df3f8aa1c27dd821798f674608f02370f05721d
ImCocos <opustoshitel228@gmail.com>
2023-09-02 20:42:43 +0300

    test commit

--- testssssss.py
+++ testssssss.py
@@ -2,5 +2,5 @@ import requests
 from pprint import pprint
 import json
 
-pprint(dict(json.loads(requests.get("https://api.openweathermap.org/data/2.5/weather?lat=55.751244&lon=37.618423&appid=594156179360808d788f634d8738d7a8&lang=ru").content.decode('utf8'))))
 
+pprint(dict(json.loads(requests.get("https://api.openweathermap.org/data/2.5/weather?lat=55.751244&lon=37.618423&appid=594156179360808d788f634d8738d7a8&lang=ru").content.decode('utf8'))))
